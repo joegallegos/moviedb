4b3b87c5a87604c77cc0e167bcbbd3cb5c40a99a
Joe Gallegos <j.gallegos25u@gmail.com>
2020-04-18 18:27:27 -0500

    init commit with base header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Alignment,
+  Navbar,
+  NavbarGroup,
+  NavbarHeading,
+} from '@blueprintjs/core';
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <Navbar className={styles.root}>
+      <NavbarGroup align={Alignment.LEFT}>
+        <NavbarHeading>Movie-DB Lookup</NavbarHeading>
+      </NavbarGroup>
+    </Navbar>
+  );
+};
+export default Header;
