81ecf26baff66d81c68c3cd2a21b1498862f9fb4
Joe Gallegos <j.gallegos25u@gmail.com>
2020-04-18 20:37:49 -0500

    update home page title cards

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { Button, Card, Elevation, Intent } from '@blueprintjs/core';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import styles from './HomePage.module.scss';
 require('dotenv').config();
 
 const KEY = process.env.REACT_APP_APIKEY;
 
-const HomePage = () => {
+const HomePage = (id) => {
   const [movies, setMovies] = useState([]);
+  const [open, setOpen] = useState(false);
 
   const fetchData = () => {
     axios
@@ -42,20 +45,38 @@ const HomePage = () => {
     fetchData();
   }, []);
 
+  const handleClick = () => {
+    setOpen({ ...id, show: !open });
+  };
+
   return (
-    <div>
+    <div className={styles.root}>
       {movies.map((movie) => (
         <div key={movie.id} className={styles.movie}>
-          <div>
-            <img
-              alt={movie.title}
-              src={`http://image.tmdb.org/t/p/w342/${movie.posterPath}`}
-            />
-          </div>
-          <div>Title: {movie.title}</div>
-          <div>Popularity: {movie.popularity}</div>
-          <div>Vote Count: {movie.voteCount}</div>
-          <div>Release Date: {movie.release}</div>
+          <Card elevation={Elevation.FOUR} className={styles.movieCard}>
+            <div className={styles.movieImage}>
+              <img
+                alt={movie.title}
+                src={`http://image.tmdb.org/t/p/w342/${movie.posterPath}`}
+              />
+            </div>
+            <div className={styles.movieInfo}>
+              <div>{movie.title}</div>
+              <div>Popularity: {movie.popularity}</div>
+              <div>Vote Count: {movie.voteCount}</div>
+              <div>Release Date: {movie.release}</div>
+              <Link to={`/movie/${movie.id}`}>
+                <Button
+                  className={styles.movieButton}
+                  small
+                  type="button"
+                  intent={Intent.PRIMARY}
+                  text="Details"
+                  onClick={handleClick}
+                />
+              </Link>
+            </div>
+          </Card>
         </div>
       ))}
     </div>
